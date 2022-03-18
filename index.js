fb694386535d116effe38b8c3627338f451b54c4
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-03-17 21:47:22 -0300

    Criando arquivos iniciais

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express'); //Importação do modulo Express
+const app = express(); //Referencia para chamar Express
+const path = require('path'); //Nescessario para adicionar arquivos estátivos.
+
+const port = 3000; //Porta do servidor
+
+app.set('view engine', 'ejs'); //Definindo o Motor de visualização
+app.use(express.static(path.join(__dirname, 'public'))); //Indicando diretorio dos arquivos estáticos
+
+app.get('/', (req, res) => {
+	res.render('index');
+}); //Arquivo a ser renderizado dentro da views
+
+app.listen(port, () =>
+	console.log(`Servidor rodando em http://localhost:${port}`)
+); //Liga o servidor na porta definida
